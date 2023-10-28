662dae6cc0da5aac91951b4a444c34d1590adead
sagar kohli <sagar.kkohli@gmail.com>
2023-10-28 20:42:16 +0530

    3 commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/Sagar-KKohli/nanodegree-capstone/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/Sagar-KKohli/nanodegree-capstone/tree/main)
 # capstone
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello World! Welcome to Udacity Capstone Project!!</h1>"
+    return "<h1>Hello World! Welcome to <b><i>Udacity Capstone Project</i></b>!!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
